4770e6655d7c72bc3d1d9f62e39bef39a92def4f
brianjASC6 <brjohns33@gmail.com>
2019-07-12 09:46:59 -0400

    Creates function with return value

---
+++ returnPractice.js
@@ -0,0 +1,7 @@
+//by default all js functions return a value. if the return value is no specified the function returns undefined.
+function someFunc(){
+return "Hello Worlewdlciccekndcvbgvcnldnweeffvvnddlkjdd";
+}
+
+const result = someFunc();
+console.group(result);
